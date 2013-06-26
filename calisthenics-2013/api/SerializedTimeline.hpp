2554239940b32e731b873e0c1c58aff8147e5c9e
Wojciech Migda <wojtek.golf@interia.pl>
2013-06-27 00:48:27 +0200

    cleanup

---
+++ calisthenics-2013/api/SerializedTimeline.hpp
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Wojciech Migda
+ * All rights reserved
+ * Distributed under the terms of the GNU LGPL v3
+ *******************************************************************************
+ *
+ * Filename: SerializedTimeline.hpp
+ *
+ * Description:
+ *      description
+ *
+ * Authors:
+ *          Wojciech Migda (wm)
+ *
+ *******************************************************************************
+ * History:
+ * --------
+ * Date         Who  Ticket     Description
+ * ----------   ---  ---------  ------------------------------------------------
+ * 2013-06-26   wm              Initial version
+ *
+ ******************************************************************************/
+
+
+#ifndef SERIALIZEDTIMELINE_HPP_
+#define SERIALIZEDTIMELINE_HPP_
+
+#include <string>
+
+class SerializedTimeline
+{
+private:
+
+public:
+    explicit SerializedTimeline(std::string string){}
+
+};
+
+
+#endif /* SERIALIZEDTIMELINE_HPP_ */
