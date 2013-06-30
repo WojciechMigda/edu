2e5c47c9c3f7107ea4d43e365bf1aac8f44649a2
Wojciech Migda <wojtek.golf@interia.pl>
2013-06-30 23:06:01 +0200

    more setup

---
+++ counted_ptr/src/counted_ptr.hpp
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * 2013 Wojciech Migda
+ * Distributed under the terms of the Public Domain license
+ *******************************************************************************
+ *
+ * Filename: counted_ptr.hpp
+ *
+ * Description:
+ *      description
+ *
+ * Authors:
+ *          Wojciech Migda (wm)
+ *
+ *******************************************************************************
+ * History:
+ * --------
+ * Date         Who  Ticket     Description
+ * ----------   ---  ---------  ------------------------------------------------
+ * 2013-06-30   wm              Initial version
+ *
+ ******************************************************************************/
+
+
+#ifndef COUNTED_PTR_HPP_
+#define COUNTED_PTR_HPP_
+
+template<class T, void Deleter(void *)=operator delete>
+class counted_ptr
+{
+    ;
+};
+
+#endif /* COUNTED_PTR_HPP_ */
